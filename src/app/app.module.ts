548668d6bb0b58881f58fd33d19eddc1674e4dbe
gustavo <gustavo.ccx@hotmail.com>
2024-07-14 11:32:29 -0300

    INT-1
    - Commit de Inicialização
    - Criado Modulo de Loja
    - Criado Modulo de Produto
    - Criado o App Routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LojaComponent } from './Loja/loja.component';
+import { ProdutoComponent } from './Produto/produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LojaComponent,
+    ProdutoComponent
   ],
   imports: [
     BrowserModule
