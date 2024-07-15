5726152ae20be564cf329b3775cb1d65ffcbec58
gustavo <gustavo.ccx@hotmail.com>
2024-07-14 23:16:19 -0300

    INT-3
    - Criação da Página de Consulta de Produtos

--- src/app/Produto/Consulta Produto/consulta-produto.component.ts
+++ src/app/Produto/Consulta Produto/consulta-produto.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-consulta-produto',
   templateUrl: './consulta-produto.component.html',
-  styleUrls: ['./consulta-produto.component.css']
+  styleUrls: ['./consulta-produto.component.css'],
 })
-export class ConsultaProdutoComponent {
-
-}
+export class ConsultaProdutoComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ConsultaProdutoComponent } from './Produto/Consulta Produto/consulta-produto.component';
 import { LojaComponent } from './Loja/loja.component';
 import { CadastroProdutoComponent } from './Produto/Cadastro Produto/cadastro-produto.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ConsultaProdutoComponent } from './Produto/Consulta Produto/consulta-produto.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LojaComponent,
-    CadastroProdutoComponent,
-    ConsultaProdutoComponent,
+  declarations: [AppComponent, LojaComponent, CadastroProdutoComponent, ConsultaProdutoComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTooltipModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
